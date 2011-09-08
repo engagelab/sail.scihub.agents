9fd00a4f1eee8b494934ffbed9679c4a1a2aff23
Anthony Perritano <aperritano@gmail.com>
2011-09-08 15:07:52 +0200

    agent persists to mongo video url

---
+++ src/sail/schihub/agents/GopherAgent.java
@@ -0,0 +1,136 @@
+package sail.schihub.agents;
+
+import java.net.UnknownHostException;
+import java.sql.Timestamp;
+import java.util.Date;
+import java.util.Set;
+
+import org.encorelab.sail.Event;
+import org.encorelab.sail.EventResponder;
+import org.encorelab.sail.agent.Agent;
+import org.jivesoftware.smack.XMPPException;
+
+import com.mongodb.BasicDBObject;
+import com.mongodb.DB;
+import com.mongodb.DBCollection;
+import com.mongodb.Mongo;
+import com.mongodb.MongoException;
+
+public class GopherAgent extends Agent {
+	
+	public static void main(String[] args) {
+		Thread thread = new Thread() {
+			@Override
+			public void run() {
+				try {
+					GopherAgent agent = new GopherAgent();
+					agent.setName("TheGopherAgent"); // can be omitted
+
+					System.out.println("Connecting...");
+					agent.connect("imediamac28.uio.no");
+
+					System.out.println("Logging in...");
+					agent.login("gopheragent",
+							"gopheragent");
+
+					System.out.println("Connecting to Mongo...");
+					agent.setupDBConnection();
+					
+					System.out.println("Setting up responders...");
+					agent.setupEventResponders();
+
+					System.out.println("Listening for Sail events...");
+					agent.listen();
+
+					
+					
+					System.out.println("Joining groupchat...");
+					agent.joinGroupchat("scihub@conference.imediamac28.uio.no");
+
+					// FIXME: is this really the best way to keep the agent alive?
+					while (true) {
+						try {
+							Thread.sleep(1000);
+						} catch (InterruptedException e) {
+							System.out.println("Agent killed.");
+						}
+					}
+				} catch (XMPPException e) {
+					System.err.println(e.getMessage());
+				}
+			}
+		};
+
+		thread.start();
+
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			System.out.println("Agent killed.");
+		}
+	}
+
+	private Mongo mongoDBConnection;
+	private DB mongoDB;
+	
+	public void setupDBConnection() {
+		
+		try {
+			mongoDBConnection = new Mongo( "imediamac28.uio.no" , 27017 );
+			mongoDB = mongoDBConnection.getDB( "scihub" );
+			
+		} catch (UnknownHostException e) {
+			e.printStackTrace();
+		} catch (MongoException e) {
+			e.printStackTrace();
+		}
+
+		
+	}
+	
+	public void printTest() {
+		System.out.println("test print....");
+		Set<String> colls = mongoDB.getCollectionNames();
+
+		for (String s : colls) {
+		    System.out.println(s);
+		}
+	}
+	
+	public void setupEventResponders() {
+		/*
+		 * When the agent sees an event like:
+		 * 
+		 * 
+		 * { eventType: 'new_video_submitted', payload: { url: 'http://youtube.com/lakjsdflkjsdfklj'}, origin: 'bob' }
+		 * 
+		 * it will respond by adding a record to mongo
+		 * 
+		 * { url: 'http://youtube.com/lakjsdflkjsdfklj', author: 'bob', timestamp: '2011-09...' }
+		 */
+		listener.addResponder("new_video_submitted", new EventResponder() {
+			public void respond(Event ev) {
+				String fromJid = ev.getFrom();
+				String fromUsername = fromJid.split("/")[1];
+				System.out.println("new_submitted_video from " + fromUsername );
+				
+				String origin = ev.getOrigin();
+				
+				String url = (String) ev.getPayloadAsMap().get("url");
+				
+				DBCollection coll = mongoDB.getCollection("videos");
+						
+		        BasicDBObject doc = new BasicDBObject();
+		        
+		        doc.put("url", url);
+		        doc.put("origin", origin);
+		        Date date = new Date();
+		        doc.put("timestamp", new Timestamp(date.getTime()));
+		        
+		        coll.insert(doc);
+		        
+			}
+		});
+
+}
+}
